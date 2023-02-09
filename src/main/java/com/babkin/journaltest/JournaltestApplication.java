c261d11e541796bb1c7ddc5e49421714befc54ed
Babkin Gennadiy <babkin@kursksu.ru>
2023-02-09 13:47:59 +0300

    1 Создание  простого Web приложения

---
+++ src/main/java/com/babkin/journaltest/JournaltestApplication.java
@@ -0,0 +1,13 @@
+package com.babkin.journaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournaltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournaltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babkin/journaltest/JournaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babkin.journaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
